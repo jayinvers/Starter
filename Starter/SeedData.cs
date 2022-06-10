d8500ee8e272368392865b8b799db4eebd9c1074
maxazure <maxazure@gmail.com>
2022-06-10 23:40:25 +1200

    Add .gitignore file to ignore Migrations folder

--- Starter/SeedData.cs
+++ Starter/SeedData.cs
@@ -7,7 +7,6 @@ namespace Starter
 {
     public static class SeedData
     {
-       
         public static void Initialize(IServiceProvider serviceProvider)
         {
             using (var context = new DatabaseContext(serviceProvider.GetRequiredService<DbContextOptions<DatabaseContext>>())){
